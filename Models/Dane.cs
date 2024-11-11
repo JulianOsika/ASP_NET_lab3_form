a62588f22beaf76ed1095391609f44bb6ccbdec3
Julian Osika <osikajulian@gmail.com>
2024-11-11 15:22:27 +0100

    pierwszy commit

---
+++ Models/Dane.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3formularz.Models
+{
+    public class Dane
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Podaj imie")]
+        [MinLength(2)]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwisko")]
+        [MinLength(2)]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Prosze podaj Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Podaj haslo")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$",
+            ErrorMessage = "Hasło musi zawierać przynajmniej jedną literę małą, dużą oraz cyfrę")]
+        public string Haslo { get; set; }
+
+        [Required(ErrorMessage = "Potwierdz haslo")]
+        [Compare("Haslo", ErrorMessage = "Hasla musza byc identyczne")]
+        public string PotwierdzHaslo { get; set; }
+
+        [Phone(ErrorMessage = "Niepoprawny numer telefonu")]
+        public string NrTelefonu { get; set; }
+
+        [Range(10, 80, ErrorMessage = "Wiek muis sie miescic miedzy 10 a 80")]
+        public int Wiek {  get; set; }
+
+        public string Miasto { get; set; }
+        public enum Kategorie { A = 1, B =2 , C = 3, D = 4, E = 5 }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab3formularz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
